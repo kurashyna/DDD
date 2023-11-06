06981cd96adf1bfef83074021aea4214e9de80b5
Lucas Le Menn <lucas.le-menn@etu.u-bordeaux.fr>
2023-11-06 17:22:42 +0100

    :sparkles: Reference.java

---
+++ src/Domain/Reference.java
@@ -0,0 +1,74 @@
+package Domain;
+
+import java.util.Objects;
+import java.util.regex.*;
+/**
+ * Value Object
+ * */
+public class Reference {
+    private final String ref;
+    private final String nom;
+    private final String description;
+    private final int prix;
+
+    public Reference(String ref, String nom, String description, int prix) {
+        Pattern p = Pattern.compile("[a-zA-Z0-9]{0,20}"); // 20 char max alphanumérique
+        Matcher m = p.matcher(ref);
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Ref doit contenir 20 char max");
+        }
+        this.ref = ref;
+
+        p = Pattern.compile("\\w{0,20}"); // 20 char max
+        m = p.matcher(nom);
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Nom doit contenir 20 char max");
+        }
+        this.nom = nom;
+
+        p = Pattern.compile("\\w{0,200}"); // 200 char max
+        m = p.matcher(description);
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Description doit contenir 200 char max");
+        }
+        this.description = description;
+
+        p = Pattern.compile("\\d*"); // Entier positif
+        m = p.matcher(String.valueOf(prix));
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Prix doit être un entier positif");
+        }
+        this.prix = prix;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reference reference = (Reference) o;
+        return prix == reference.prix && Objects.equals(ref, reference.ref) && Objects.equals(nom, reference.nom) && Objects.equals(description, reference.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ref, nom, description, prix);
+    }
+
+
+}
